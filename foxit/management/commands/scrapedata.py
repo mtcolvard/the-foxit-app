f0ade0cfc0a75c398ee5e61be83d10bf7024193f
mtcolvard <matthew.colvard@gmail.com>
2019-09-11 18:37:56 +0100

    inserted new models

--- foxit/management/commands/scrapedata.py
+++ foxit/management/commands/scrapedata.py
@@ -11,12 +11,44 @@ class Command(BaseCommand):
         # x = urllib.request.urlopen('http://www.londongardensonline.org.uk/search-advanced-results.php?borough=%25&type=%25&keyword=&Submit=Search')
         # print(x.read())
         soup = BeautifulSoup(x, 'html.parser')
+
+        citymapper_href = soup.find(id='basic').a.get('href')
+        citymapper_href_edit1 = citymapper_href.replace('http://citymapper.com/directions?endcoord=', '')
+        sep = '&'
+        citymapper_href_edit2 = (citymapper_href_edit1.split(sep, 1)[0]).split(',')
+        citymapper_href_edit3 = [float(i) for i in citymapper_href_edit2]
+        citymapper_href_edit3.reverse()
+        citymapper_lon = citymapper_href_edit3.pop(0)
+        citymapper_lat = citymapper_href_edit3.pop(0)
+        image = ([str(soup.find(id='photos').img)].pop().split('src="', 1)[1]).replace('"/>','')
+        image_formatted = ''.join(['http://www.londongardensonline.org.uk/', image])
+
         data = {
             'name': soup.title.string,
-            'endname': soup.title.string,
-            'endcoord': soup.title.string,
-            'lat': 51.5555,
-            'lon': 0.0000
+            'summary': soup.find(id='summary').p.string,
+            'previous_name': soup.find(id='basic').select("dt ~ dd:nth-of-type(1)")[0].string,
+            'site_location': soup.find(id='basic').select("dt ~ dd:nth-of-type(2)")[0].string,
+            'postcode': soup.find(id='basic').select("dt ~ dd:nth-of-type(3)")[0].string.rstrip(),
+            'type_of_site': soup.find(id='basic').select("dt ~ dd:nth-of-type(4)")[0].string,
+            'date': soup.find(id='basic').select("dt ~ dd:nth-of-type(5)")[0].string,
+            'designer': soup.find(id='basic').select("dt ~ dd:nth-of-type(6)")[0].string,
+            'listed_structures': soup.find(id='basic').select("dt ~ dd:nth-of-type(7)")[0].string,
+            'borough': soup.find(id='basic').select("dt ~ dd:nth-of-type(8)")[0].string,
+            'site_ownership': soup.find(id='basic').select("dt ~ dd:nth-of-type(9)")[0].string,
+            'site_management': soup.find(id='basic').select("dt ~ dd:nth-of-type(10)")[0].string,
+            'open_to_public': soup.find(id='basic').select("dt ~ dd:nth-of-type(11)")[0].string.rstrip(),
+            'opening_times': soup.find(id='basic').select("dt ~ dd:nth-of-type(12)")[0].string.lstrip(),
+            'special_conditions': soup.find(id='basic').select("dt ~ dd:nth-of-type(13)")[0].string,
+            'facilities': soup.find(id='basic').select("dt ~ dd:nth-of-type(14)")[0].string,
+            'public_transportation': soup.find(id='basic').select("dt > dd")[1].string,
+            'lon_lat': citymapper_href_edit3,
+            'lon': citymapper_lon,
+            'lat': citymapper_lat,
+            'grid_reference': soup.find(id='further').select("dt ~ dd:nth-of-type(1)")[0].string.rstrip(),
+            'size_in_hectares': soup.find(id='further').select("dt ~ dd:nth-of-type(2)")[0].string,
+            'image': image_formatted,
+            'fuller_information': soup.find(id='fuller').p.get_text(),
+            'sources_consulted': soup.find(id='fuller').select("section > p")[1].string,
         }
 
         location = Location(**data)
--- foxit/models.py
+++ foxit/models.py
@@ -3,10 +3,30 @@ from django.db import models
 # Create your models here.
 class Location(models.Model):
     name = models.CharField(max_length=100)
-    endname = models.CharField(max_length=100)
-    endcoord = models.CharField(max_length=100)
-    lat = models.FloatField()
+    summary = models.TextField(max_length=2500)
+    previous_name = models.CharField(max_length=100)
+    site_location = models.CharField(max_length=100)
+    postcode = models.CharField(max_length=10)
+    type_of_site = models.CharField(max_length=50)
+    date = models.CharField(max_length=30)
+    designer = models.CharField(max_length=50)
+    listed_structures = models.CharField(max_length=150)
+    borough = models.CharField(max_length=30)
+    site_ownership = models.CharField(max_length=50)
+    site_management = models.CharField(max_length=50)
+    open_to_public = models.CharField(max_length=10)
+    opening_times = models.CharField(max_length=30)
+    special_conditions = models.CharField(max_length=250)
+    facilities = models.CharField(max_length=250)
+    public_transportation = models.CharField(max_length=200)
+    lon_lat = models.FloatField()
     lon = models.FloatField()
+    lat = models.FloatField()
+    grid_reference = models.CharField(max_length=30)
+    size_in_hectares = models.CharField(max_length=100)
+    image = models.CharField(max_length=250)
+    fuller_information = models.TextField(max_length=10000)
+    sources_consulted = models.TextField(max_length=1000)
 
     def __str__(self):
         return f'{self.name}'
