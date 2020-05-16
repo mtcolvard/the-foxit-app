060f38b8abba7cff393af2c409c29b18677ab8de
mtcolvard <matthew.colvard@gmail.com>
2020-05-16 21:23:57 +0200

    onclick working as an event in dropdown menu but not sending data

--- foxit/views.py
+++ foxit/views.py
@@ -64,25 +64,25 @@ class MapDirectionsView(APIView):
             'access_token': 'pk.eyJ1IjoibXRjb2x2YXJkIiwiYSI6ImNrMDgzYndkZjBoanUzb21jaTkzajZjNWEifQ.ocEzAm8Y7a6im_FVc92HjQ'
         }
         response = requests.get(f'https://api.mapbox.com/directions/v5/mapbox/walking/{coords}', params=params)
-        print(response.json())
-        return Response(response.json())
-
-class MapGeocoderView(APIView):
-    def get(self, _request, searchQuery, bbox=None, country='GB'):
-        params = {
-            # 'limit': 1,
-            'country': {country},
-            'access_token': 'pk.eyJ1IjoibXRjb2x2YXJkIiwiYSI6ImNrMDgzYndkZjBoanUzb21jaTkzajZjNWEifQ.ocEzAm8Y7a6im_FVc92HjQ'
-        }
-        response = requests.get(f'https://api.mapbox.com/geocoding/v5/mapbox.places/{searchQuery}.json', params=params)
-        data = response.json()
-        # print(data)
+        # print(response.json())
         return Response(response.json())
 
 # class MapGeocoderView(APIView):
-#     def get(self, _request, coords, bbox=None, country='ISO 3166-2:GB'):
-#         geocoder = Geocoder(name='mapbox.places', access_token='pk.eyJ1IjoibXRjb2x2YXJkIiwiYSI6ImNrMDgzYndkZjBoanUzb21jaTkzajZjNWEifQ.ocEzAm8Y7a6im_FVc92HjQ')
-#         response = geocoder.forward(coords, bbox, country)
+#     def get(self, _request, searchQuery, bbox=None, country='GB'):
+#         params = {
+#             # 'limit': 1,
+#             'country': {country},
+#             'access_token': 'pk.eyJ1IjoibXRjb2x2YXJkIiwiYSI6ImNrMDgzYndkZjBoanUzb21jaTkzajZjNWEifQ.ocEzAm8Y7a6im_FVc92HjQ'
+#         }
+#         response = requests.get(f'https://api.mapbox.com/geocoding/v5/mapbox.places/{searchQuery}.json', params=params)
 #         data = response.json()
 #         print(data)
 #         return Response(response.json())
+
+class MapGeocoderView(APIView):
+    def get(self, _request, searchQuery, bbox=None, country='ISO 3166-2:GB'):
+        geocoder = Geocoder(name='mapbox.places', access_token='pk.eyJ1IjoibXRjb2x2YXJkIiwiYSI6ImNrMDgzYndkZjBoanUzb21jaTkzajZjNWEifQ.ocEzAm8Y7a6im_FVc92HjQ')
+        response = geocoder.forward(searchQuery, bbox, country)
+        data = response.json()
+        print(data)
+        return Response(response.json())
